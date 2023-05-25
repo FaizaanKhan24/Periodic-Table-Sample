bd7c6fd204ccec286640766d4b2c728e1cec1867
Faizaan Khan <faizaankhan24@gmail.com>
2023-05-25 18:27:48 -0400

    Adding few components according to requirements

---
+++ src/Components/ElementDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ElementDetail.css';
+
+function ElementDetail() {
+  return (
+    <div>ElementDetail</div>
+  )
+}
+
+export default ElementDetail
---
+++ src/Components/ElementsRow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ElementsRow.css';
+
+function ElementsRow() {
+  return (
+    <div>ElementsRow</div>
+  )
+}
+
+export default ElementsRow
