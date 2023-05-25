8768fa83395b86a9420304bec50deae48907e47b
Faizaan Khan <faizaankhan24@gmail.com>
2023-05-25 18:28:23 -0400

    Creating a base structure for atomic structure

---
+++ src/Components/AtomicStructure.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './AtomicStructure.css';
+
+const AtomicStructure = () => {
+  return (
+    <div className="container">
+      <div className="p1-orbital">
+        <div className="s2-orbital">
+          <div className="s1-orbital">
+            <div className="nucleus">
+              <div className="protons">+12</div>
+              <div className="s1-1 electron">.</div>
+              <div className="s1-2 electron">.</div>
+              <div className="s2-1 electron">.</div>
+              <div className="s2-2 electron">.</div>
+              <div className="p2-1 electron">.</div>
+              <div className="p2-2 electron">.</div>
+              <div className="p2-3 electron">.</div>
+              <div className="p2-4 electron">.</div>
+              <div className="p2-5 electron">.</div>
+              <div className="p2-6 electron">.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AtomicStructure
