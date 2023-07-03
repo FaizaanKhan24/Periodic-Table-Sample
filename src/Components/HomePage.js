5edc5201e8597fa4fcb44797ca9e4d4d71f1cd09
Faizaan Khan <faizaankhan24@gmail.com>
2023-07-03 08:46:49 -0700

    Adding elements to the home page

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './HomePage.css'
-import AtomicStructure from './AtomicStructure'
+import AtomicStructure from './AtomicStructure';
+import ElementRow from './ElementsRow';
 
 function HomePage() {
   return (
     <div className="container">
         <div className="left"></div>
         <div className="right">
+            <ElementRow/>
             <AtomicStructure/>
         </div>
     </div>
