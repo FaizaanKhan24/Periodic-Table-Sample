ff0c59a1e0136c708d151a87bf1fce400ea277f9
Faizaan Khan <faizaankhan24@gmail.com>
2023-08-07 21:23:39 -0700

    Adding header and F-elements
    - Adding the column number using columnNumber function
    - Filtering the elements depending on their atomic number and adding them into the periodic table.
    - elements and extendedElements take care of the task

--- src/Components/ElementsRow.js
+++ src/Components/ElementsRow.js
@@ -7,13 +7,42 @@ function ElementsRow({selectedElement, onChangeSelectedElement}) {
     onChangeSelectedElement(elementSelected)
   }
 
-  const elements = elementData.elements.map(element => 
+  const isExtendedElement = (value) => {
+    // Lanthanoid series
+    if (value.atomicNumber >= 58 && value.atomicNumber <= 71)
+      return true
+    // Actinoid series
+    if (value.atomicNumber >= 90 && value.atomicNumber <= 103)
+      return true
+    return false
+  }
+
+  const columnNumber =  () => {
+    const columnItems = []
+    for(let i=1; i<19; i++){
+      columnItems.push(<label className="columnNumber" key={i}>{i}</label>)
+    }
+
+    return (<div className="columnNumberContainer">{columnItems}</div>)
+  }
+
+  const elements = elementData.elements.filter(val => !isExtendedElement(val)).map(element => 
+    <button key={element.atomicNumber} style={element.grid} className="element" onClick={handleElementClicked(element)}>{element.symbol}</button>
+  );
+
+  const extendedElements = elementData.elements.filter(isExtendedElement).map(element => 
     <button key={element.atomicNumber} style={element.grid} className="element" onClick={handleElementClicked(element)}>{element.symbol}</button>
   );
 
   return (
-    <div className="elementsContainer">
-      {elements}
+    <div>
+      {columnNumber()}
+      <div className="elementsContainer">
+        {elements}
+      </div>
+      <div className='extraElementsContainer'>
+        {extendedElements}
+      </div>
     </div>
   )
 }
