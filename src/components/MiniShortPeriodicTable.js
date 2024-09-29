ab2fb72b57210fd1aeefdaa2c4368d551e08cc53
Faizaan Khan <faizaankhan24@gmail.com>
2024-09-28 23:29:06 -0700

    Basic cleanup and alignment

--- src/components/MiniShortPeriodicTable.js
+++ src/components/MiniShortPeriodicTable.js
@@ -17,15 +17,6 @@ function MiniShortPeriodicTable({selectedElement, onChangeSelectedElement}) {
     return false
   }
 
-  const columnNumber =  () => {
-    const columnItems = []
-    for(let i=1; i<19; i++){
-      columnItems.push(<label className="columnNumber" key={i}>{i}</label>)
-    }
-
-    return (<div className="columnNumberContainer">{columnItems}</div>)
-  }
-
   const elements = elementData.elements.filter(val => !isExtendedElement(val)).map(element => 
     <button key={element.atomicNumber} style={element.grid} className="element" onClick={handleElementClicked(element)}/>
   );
@@ -35,7 +26,6 @@ function MiniShortPeriodicTable({selectedElement, onChangeSelectedElement}) {
   );
   return (
     <div>
-      {/* {columnNumber()} */}
       <div className="elementsContainer">
         {elements}
       </div>
